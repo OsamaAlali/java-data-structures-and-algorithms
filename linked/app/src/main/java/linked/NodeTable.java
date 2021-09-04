8a410451b712f3672c96be0e9b07e50871cb1771
OsamaAlali <osama.alali89@yahoo.com>
2021-09-04 18:27:34 +0300

    ch30

--- linked/app/src/main/java/linked/NodeTable.java
+++ linked/app/src/main/java/linked/NodeTable.java
@@ -1,20 +1,3 @@
 package linked;
 
-public class NodeTable<T> {
-int key;
-T value;
-NodeTable next ;
 
-    public NodeTable(int key, T value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public int getKey() {
-        return key;
-    }
-
-    public T getValue() {
-        return value;
-    }
-}
